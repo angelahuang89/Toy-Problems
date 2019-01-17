18a8cb2569bba58a4246a57cea162f858078b8e8
Angela Huang <angelahuang89@gmail.com>
2019-01-17 10:04:01 -0800

    Complete valid parenthesis string

--- validParenthesisString.js
+++ validParenthesisString.js
@@ -1,43 +1,85 @@
 const checkValidString = s => {
-  if (!s.length) return true;
-  const counts = [];
+  let low = 0;
+  let high = 0;
   for (let i = 0; i < s.length; i++) {
     const curr = s[i];
     if (curr === '(') {
-      if (!counts.length) {
-        counts.push(1);
-      } else {
-        for (let j = 0; j < counts.length; j++) {
-          counts[j]++;
-        }
-      }
+      low++;
+      high++;
+    } else if (curr === ')') {
+      if (low > 0) low--;
+      high--;
+    } else {
+      if (low > 0) low--;
+      high++;
     }
-    if (curr === ')') {
-      for (let k = 0; k < counts.length; k++) {
-        if (counts[k] > 0) {
-          counts[k]--;
-        } else {
-          counts[k] = 'not valid';
-        }
-      }
-    }
-    if (curr === '*') {
-      const copy = counts.slice();
-      if (!copy.length) {
-        counts.push(0);
-        counts.push(1);
-      } else {
-        for (let l = 0; l < copy.length; l++) {
-          const curr = copy[l];
-          counts.push(curr + 1);
-          if (curr > 0) counts.push(curr - 1);
-          counts.push(curr);
-        }
-      }
-    }
+    if (high < 0) return false;
   }
-  return counts.indexOf(0) > -1;
+  return low === 0;
 }
+
+// const checkValidString = s => {
+//   const openStack = [];
+//   const wildStack = [];
+//   for (let i = 0; i < s.length; i++) {
+//     const curr = s[i];
+//     if (curr === '(') {
+//       openStack.push(curr);
+//     }
+//     if (curr === ')') {
+//       if (openStack.length) {
+//         openStack.pop();
+//       } else if (wildStack.length) {
+//         wildStack.pop();
+//       }
+//     }
+//     if (curr === '*') {
+//       wildStack.push(curr);
+//     }
+//   }
+//   return openStack.length === 0 || wildStack.length >= openStack.length;
+// };
+
+// const checkValidString = s => {
+//   if (!s.length) return true;
+//   const counts = [];
+//   for (let i = 0; i < s.length; i++) {
+//     const curr = s[i];
+//     if (curr === '(') {
+//       if (!counts.length) {
+//         counts.push(1);
+//       } else {
+//         for (let j = 0; j < counts.length; j++) {
+//           counts[j]++;
+//         }
+//       }
+//     }
+//     if (curr === ')') {
+//       for (let k = 0; k < counts.length; k++) {
+//         if (counts[k] > 0) {
+//           counts[k]--;
+//         } else {
+//           counts[k] = 'not valid';
+//         }
+//       }
+//     }
+//     if (curr === '*') {
+//       const copy = counts.slice();
+//       if (!copy.length) {
+//         counts.push(0);
+//         counts.push(1);
+//       } else {
+//         for (let l = 0; l < copy.length; l++) {
+//           const curr = copy[l];
+//           counts.push(curr + 1);
+//           if (curr > 0) counts.push(curr - 1);
+//           counts.push(curr);
+//         }
+//       }
+//     }
+//   }
+//   return counts.indexOf(0) > -1;
+// };
 
 // const checkValidString = s => {
 //   let openCount = 0;
@@ -93,15 +135,15 @@ const checkValidString = s => {
 
 // implementation
 
-// console.log(checkValidString('()')); // true
-// console.log(checkValidString('(*)')); // true
-// console.log(checkValidString('(*))')); // true
-// console.log(checkValidString(')(')); // false
-// console.log(checkValidString('(')); // false
-// console.log(checkValidString(')')); // false
-// console.log(checkValidString('(((******))')); // true
-// console.log(checkValidString('')); // true
-// console.log(checkValidString('*')); // true
-// console.log(checkValidString('(((((()*)(*)*))())())(()())())))((**)))))(()())()')); // false
-// console.log(checkValidString('(())((())()()(*)(*()(())())())()()((()())((()))(*')) // false
-console.log(checkValidString('(())()())(*))(((((())()*))**))**()(*(()()*)(**(())()**)((**(()(((()()**)())*(*))(())()()*'));
+console.log(checkValidString('()')); // true
+console.log(checkValidString('(*)')); // true
+console.log(checkValidString('(*))')); // true
+console.log(checkValidString(')(')); // false
+console.log(checkValidString('(')); // false
+console.log(checkValidString(')')); // false
+console.log(checkValidString('(((******))')); // true
+console.log(checkValidString('')); // true
+console.log(checkValidString('*')); // true
+console.log(checkValidString('(((((()*)(*)*))())())(()())())))((**)))))(()())()')); // false
+console.log(checkValidString('(())((())()()(*)(*()(())())())()()((()())((()))(*')) // false
+console.log(checkValidString('(())()())(*))(((((())()*))**))**()(*(()()*)(**(())()**)((**(()(((()()**)())*(*))(())()()*')); // false
